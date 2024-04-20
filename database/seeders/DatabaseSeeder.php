94448b9020c4c735aeefc81d1745c40ee2b135e3
Benjamin.M <benjmonte@outlook.fr>
2024-04-20 17:37:47 +0200

    Added customer seeder default

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Webkul\Installer\Database\Seeders\DatabaseSeeder as BagistoDatabaseSeeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,5 +19,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(BagistoDatabaseSeeder::class);
+
+        DB::table('customers')->insert([
+            'first_name'              => "Classic User",
+            'last_name'              => "Classic User",
+            'email'             => "user@example.com",
+            'remember_token'    => Str::random(10),
+            'gender'            => Arr::random(['male', 'female', 'other']),
+            'status'            => 1,
+            'password'          => Hash::make('password'),
+            'customer_group_id' => 2,
+            'is_verified'       => 1,
+            'created_at'        => now(),
+            'updated_at'        => now(),
+        ]);
     }
 }
